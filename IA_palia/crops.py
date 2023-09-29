f44d54e3c514206031787f3f8c5ce5d9548c9d5a
Peraefe <fefeple106@gmail.com>
2023-09-29 20:32:34 -0300

    mudar nome da pasta

---
+++ IA_palia/crops.py
@@ -0,0 +1,58 @@
+carrot = {
+    'id': 1,
+    'bonus': None,
+    'price': 23,
+    'time': 3,
+    'qty': 2
+}
+corn = {
+    'id': 2,
+    'bonus': 'qualidade',
+    'price': 40,
+    'time': 5,
+    'qty': 2
+}
+cotton = {
+    'id': 3,
+    'bonus': 'qualidade',
+    'price': 45,
+    'time': 5,
+    'qty': 2
+}
+onion = {
+    'id': 4,
+    'bonus': None,
+    'price': 30,
+    'time': 4,
+    'qty': 2
+}
+potato = {
+    'id': 5,
+    'bonus': None,
+    'price': 45,
+    'time': 5,
+    'qty': 2
+}
+rice = {
+    'id': 6,
+    'bonus': 'quantidade',
+    'price': 27,
+    'time': 3,
+    'qty': 2
+}
+tomato = {
+    'id': 7,
+    'bonus': None,
+    'price': 23,
+    'time': 10,
+    'qty': 8
+}
+wheat = {
+    'id': 8,
+    'bonus': 'quantidade',
+    'price': 33,
+    'time': 4,
+    'qty': 2
+}
+
+crops = [carrot, corn, cotton, onion, potato, rice, tomato, wheat]
---
+++ IA_palia/percent.py
@@ -0,0 +1,4 @@
+noBonus = [2,2.5]
+qualityBonus = [2,2.5,2.5,2.5,2.5]
+quantityBonus = [3,3,3.5,4]
+bothBonus = [3,3,3.5,3.5,3.5,4,4,4,4.5,4.5]
